0f271d81723e0ca226898d4b4c2fab83e9660ad1
mabart <bartkowiak.mateusz88@gmail.com>
2021-05-25 00:01:13 +0200

    Update run2.js

--- run2.js
+++ run2.js
@@ -18,7 +18,7 @@
 </head>
 <body>
     <div class="container">
-        <h1>SFMC Custom Scripts Installer</h1>
+        <h1>SFMC Custom Scripts Install</h1>
         <p>Created by <a href="https://www.linkedin.com/in/mateusz-bartkowiak-9b865165/" target="_blank">Mateusz Bartkowiak</a>. Reach out with bugs and requests.</p>
         <script runat="server">
         //  NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
@@ -814,7 +814,11 @@
                 var obj = proxy.createItem(type, definition);
     
                 if (obj.Results.length == 1 && obj.Status == 'OK') {
-    
+
+                    if (obj.Results[0].NewObjectID) {
+                        retrievableObjectData[definition.Name + type] = obj.Results[0].NewObjectID;
+                    }
+                    
                     if (definition.Name.indexOf('Watermark') > -1 && type == 'DataExtension') {
                         addWatermarkRecord(definition.CustomerKey);
                     }
@@ -832,7 +836,7 @@
         function createAutomationFromDefinition(definition) {
             for (var i = 0; i < definition.AutomationTasks.length; i ++) {
                 for (var j = 0; j < definition.AutomationTasks[i].Activities.length; j ++) {
-                    //definition.AutomationTasks[i].Activities[j].ObjectID = retrievableObjectData[definition.AutomationTasks[i].Activities[j].Name + 'QueryDefinition'];
+                    definition.AutomationTasks[i].Activities[j].ObjectID = retrievableObjectData[definition.AutomationTasks[i].Activities[j].Name + 'QueryDefinition'];
                     definition.AutomationTasks[i].Activities[j].ActivityObject.ObjectID = retrievableObjectData[definition.AutomationTasks[i].Activities[j].Name + 'QueryDefinition'];
                 }
             }
