39310a4a28e2dcbba55059d1da532355cf82fd37
mabart <bartkowiak.mateusz88@gmail.com>
2021-05-25 22:18:11 +0200

    Update run2.js

--- run2.js
+++ run2.js
@@ -1050,7 +1050,7 @@
                     </div>
                 </div>
             </div>
-            <div class="accordion-item">
+            <!--<div class="accordion-item">
                 <h2 class="accordion-header" id="headingTwo">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                     Script 2
@@ -1073,7 +1073,7 @@
                         <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                     </div>
                 </div>
-            </div>
+            </div>-->
         </div>
         <!--ACCORDION END-->
         <script runat="server">
